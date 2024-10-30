5a109fe3f16dc438f422e33507a4924460ace4ea
Ashitom <sasukeuchia32828@gmail.com>
2024-10-30 18:48:59 +0545

    Add Bottom Navigation Bar For : Feed and Post Creation

--- lib/components/screens/home/homescreen.dart
+++ lib/components/screens/home/homescreen.dart
@@ -1,13 +1,22 @@
+import "package:flutter/cupertino.dart";
 import "package:flutter/material.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:social/components/controllers/authcontroller.dart";
 import "package:social/components/screens/home/delegates/searchcommunity.dart";
 import "package:social/components/screens/home/drawers/communitylist.dart";
 import "package:social/components/screens/home/drawers/userprofiledrawer.dart";
+import "package:social/core/constants.dart";
+import "package:social/themes/themehandler.dart";
 
-class HomeScreen extends ConsumerWidget {
+class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
 
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends ConsumerState<HomeScreen> {
+  int _pageNo = 0;
   void openDrawer(BuildContext context) {
     Scaffold.of(context).openDrawer();
   }
@@ -16,9 +25,16 @@ class HomeScreen extends ConsumerWidget {
     Scaffold.of(context).openEndDrawer();
   }
 
+  void onPageChange(int page) {
+    setState(() {
+      _pageNo = page;
+    });
+  }
+
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     final user = ref.watch(userDataProvider)!;
+    final currentTheme = ref.watch(themeDataProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -54,8 +70,34 @@ class HomeScreen extends ConsumerWidget {
           })
         ],
       ),
+      body: IndexedStack(
+        index: _pageNo,
+        children: Constants.tabWidgets,
+      ),
       drawer: const CommunityLists(),
       endDrawer: const ProfileDrawer(),
+      bottomNavigationBar: CupertinoTabBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Padding(
+              padding: EdgeInsets.all(16),
+              child: Icon(Icons.home),
+            ),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Padding(
+              padding: EdgeInsets.all(16),
+              child: Icon(Icons.add),
+            ),
+            label: '',
+          ),
+        ],
+        activeColor: currentTheme.iconTheme.color,
+        backgroundColor: currentTheme.dialogBackgroundColor,
+        onTap: onPageChange,
+        currentIndex: _pageNo,
+      ),
     );
   }
 }
---
+++ lib/core/constants.dart
@@ -0,0 +1,6 @@
+import 'package:social/components/screens/posts/addpostscreen.dart';
+import 'package:social/components/screens/posts/feedscreen.dart';
+
+class Constants {
+  static const tabWidgets = [AddPostScreen(), FeedScreen()];
+}
